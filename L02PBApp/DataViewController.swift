9487065994546f2934f913d8d169eb5db9bd2c98
liu gang <948514500@qq.com>
2015-11-03 23:07:51 +0900

    Initial Commit

---
+++ L02PBApp/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  L02PBApp
+//
+//  Created by liugang on 15/11/3.
+//  Copyright © 2015年 liugang. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
